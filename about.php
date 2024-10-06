480ee7f184ee1b53ef99f664aa9549e29e3efe70
Nurwahyudi <155080160+DacynxVoid@users.noreply.github.com>
2024-10-06 08:40:28 +0700

    Create about.php

---
+++ about.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'config.php';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>CRUD DENGAN PHP MySQLi</title>
+
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>INI HALAMAN ABOUT</h1>
+	
+</body>
+</html>
